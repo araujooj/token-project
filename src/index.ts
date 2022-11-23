ccedad40d800247e3b8330eead5b41b7e18a58f9
araujooj <gabrielrudy575@gmail.com>
2022-11-23 09:28:17 -0300

    app

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import {
+  ColorAmber100,
+  ColorAmber200,
+  ColorAmber300,
+  ColorAmber400,
+} from "../build/js";
+
+const colors = {
+  amber100: ColorAmber100,
+  amber200: ColorAmber200,
+  amber300: ColorAmber300,
+  amber400: ColorAmber400,
+};
+
+export { colors };
